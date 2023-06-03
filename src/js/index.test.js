8f1966a72bf2a2ff780def92719be06a18413b99
Alga Mau <122839503+MaoShizhong@users.noreply.github.com>
2023-06-04 00:07:51 +0100

    Update tests to return note frequency

--- src/js/index.test.js
+++ src/js/index.test.js
@@ -1,19 +1,22 @@
-import { getNote, splitToSingleUnit, interpretOctave, interpretNote, interpretDuration } from './test-funcs.js';
+import { getFreq } from './test-funcs.js';
 
 describe('identifies note names and respective frequencies', () => {
-    it('identifies "A" with a default octave as note 1', () => {
-        expect(getNote('A')).toBe(1);
+    it('associates A440 with a frequency of 440Hz', () => {
+        expect(getFreq('A')).toBe(440);
     });
-    it('identifies "C" with a default octave to be 4', () => {
-        expect(getNote('C')).toBe(4);
+    it('associates 3 semitones above A440 with a frequency of 523.25Hz', () => {
+        expect(getFreq('C')).toBeCloseTo(523.25, 2);
     });
-    it('identifies "G" with a default octave to be 11', () => {
-        expect(getNote('G')).toBe(11);
+    it('associates 3 semitones below A440 with a frequency of 369.99Hz (F#)', () => {
+        expect(getFreq('F#', -1)).toBeCloseTo(369.99, 2);
     });
-    it('identifies "D#" with a default octave to be 7', () => {
-        expect(getNote('D#')).toBe(7);
+    it('associates 3 semitones below A440 with a frequency of 369.99Hz (Gb)', () => {
+        expect(getFreq('G-', -1)).toBeCloseTo(369.99, 2);
     });
-    it('identifies "G-" with a default octave to be 10', () => {
-        expect(getNote('G-')).toBe(10);
+    it('associates 12 semitones below A440 with a frequency of 220Hz', () => {
+        expect(getFreq('A', -1)).toBe(220);
+    });
+    it('associates 26 semitones above A440 with a frequency of 1975.53Hz', () => {
+        expect(getFreq('B', 2)).toBeCloseTo(1975.53, 2);
     });
 });
