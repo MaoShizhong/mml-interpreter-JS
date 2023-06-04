42ae10c572d8a47bcf809db5ff5d17abad1808e2
Alga Mau <122839503+MaoShizhong@users.noreply.github.com>
2023-06-04 01:16:05 +0100

    Write tests for full string splitting

--- src/js/index.test.js
+++ src/js/index.test.js
@@ -1,19 +1,37 @@
-import { constructNoteUnit } from './test-funcs.js';
+import { splitStrToUnits } from './test-funcs.js';
 
-describe('Split string into note name and duration, then process to array', () => {
-    it('A4 gives 440Hz, 600ms', () => {
-        expect(constructNoteUnit('A4')).toEqual([440, 600]);
+describe('Takes a multi-unit string and returns an array of each note unit as str', () => {
+    it('splits a 3 note string\n"C4D#8.A2" -> ["C4", "D#8.", "A2"]', () => {
+        expect(splitStrToUnits('C4D#8.A2')).toEqual(["C4", "D#8.", "A2"]);
     });
-    it('C16. gives ~523.25Hz, 225ms', () => {
-        expect(constructNoteUnit('C16.')).toEqual([523.2511306011972, 225]);
+    it('splits a 7 note string\n"C4D#8.R8R8A-2.B16A2" -> ["C4", "D#8.", "R8", "R8", "A-2.", "B16", "A2"]', () => {
+        expect(splitStrToUnits('C4D#8.R8R8A-2.B16A2')).toEqual(["C4", "D#8.", "R8", "R8", "A-2.", "B16", "A2"]);
     });
-    it('F#8. gives ~739.99Hz, 450ms', () => {
-        expect(constructNoteUnit('F#8.')).toEqual([739.9888454232689, 450]);
+    it('splits a 3 note string with octave changes\n"C4>D#8.A2" -> ["C4", ">", "D#8.", "A2"]', () => {
+        expect(splitStrToUnits('C4D#8.A2')).toEqual(["C4", ">", "D#8.", "A2"]);
     });
-    it('E-2. gives ~622.25Hz, 1800ms', () => {
-        expect(constructNoteUnit('E-2.')).toEqual([622.2539674441618, 1800]);
+    it('splits a 3 note string with length locks\n"C4D#8.L2A" -> ["C4", "D#8.", "L2", "A"]', () => {
+        expect(splitStrToUnits('C4D#8.L2A')).toEqual(["C4", "D#8.", "L2", "A"]);
+    });
+    it('splits a 7 note string with multiple octave changes and length locks\n"L4.CD#L8R<RA-2.B16>A2" -> ["L4.", "C", "D#", "L8", "R", "<", "R", "A-2.", "B16", ">", "A2]', () => {
+        expect(splitStrToUnits('L4.CD#L8R<RA-2.B16>A2')).toEqual(["L4.", "C", "D#", "L8", "R", "<", "R", "A-2.", "B16", ">", "A2"]);
     });
 });
+
+// describe('Split string into note name and duration, then process to array', () => {
+//     it('A4 gives 440Hz, 600ms', () => {
+//         expect(constructNoteUnit('A4')).toEqual([440, 600]);
+//     });
+//     it('C16. gives ~523.25Hz, 225ms', () => {
+//         expect(constructNoteUnit('C16.')).toEqual([523.2511306011972, 225]);
+//     });
+//     it('F#8. gives ~739.99Hz, 450ms', () => {
+//         expect(constructNoteUnit('F#8.')).toEqual([739.9888454232689, 450]);
+//     });
+//     it('E-2. gives ~622.25Hz, 1800ms', () => {
+//         expect(constructNoteUnit('E-2.')).toEqual([622.2539674441618, 1800]);
+//     });
+// });
 
 // describe('identifies note names and respective frequencies', () => {
 //     it('calculates a duration of "4" with default tempo as 600ms', () => {
