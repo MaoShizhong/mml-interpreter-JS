65db3d6e82919a33cb4d1681f39e06f0f76a4cb8
Alga Mau <122839503+MaoShizhong@users.noreply.github.com>
2023-06-04 02:47:17 +0100

    Process string input in textbox
    Sanitises input and processes a fully valid MML string.
    Currently does not do anything other than console.logs each unit after
    processing.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
+import { processMML } from './note-processing.js';
 import '../css/style.css';
+
+const playBtn = document.querySelector('button');
+playBtn.addEventListener('click', () => {
+    const MML = document.querySelector('textarea').value.toUpperCase();
+    processMML(MML);
+});
---
+++ src/js/note-processing.js
@@ -0,0 +1,95 @@
+export function processMML(str) {
+    str = removeIncompatibleChars(str);
+    const units = splitStrToUnits(str);
+
+    let tempo = 100;
+    let currentOctave = 0;
+    let currentNoteDuration = 4;
+    console.log(units);
+
+    units.forEach(unit => {
+        console.log(unit);
+        // * adjusting "global" musical params as above
+        switch (unit[0]) {
+            case 'O':
+                currentOctave = parseInt(unit.slice(1)) - 4;
+                console.log({ currentOctave });
+                break;
+            case '>':
+                currentOctave++;
+                console.log({ currentOctave });
+                break;
+            case '<':
+                currentOctave--;
+                console.log({ currentOctave });
+                break;
+            case 'L':
+                const len = unit.slice(1);
+                currentNoteDuration = len.includes('.') ? parseInt(len) / 1.5 : parseInt(len);
+                console.log({ currentNoteDuration });
+                break;
+            case 'T':
+                tempo = unit.slice(1);
+                console.log({ tempo });
+                break;
+            default:
+                console.log((constructNoteUnit(unit, currentOctave, tempo, currentNoteDuration)));
+                break;
+        }
+    });
+}
+
+// function playNote(noteUnit) {
+
+// }
+
+function removeIncompatibleChars(str) {
+    // * remove non-MML characters
+    // * as well as nonsensical combinations e.g. A. (remove only the .) / T# / L / C8- (replace only the -) etc. 
+    return str.replaceAll(/(V\d*\.?)|[H-QS-Z][.#+\-]*(?!\d+)|[\s!"£$%^&*()_=?:;@'~\[\]{}`¬\\|]|(?<=[A-Z+#\-])\.+|(?<=\d)[+#\-]/g, '');
+}
+
+function splitStrToUnits(str) {
+    return str.match(/([A-Z]-?\+?#?\d*\.?)|<|>/g);
+}
+
+function constructNoteUnit(str, currentOctave = 0, tempo = 100, currentNoteLength) {
+    const noteName = str.match(/[A-Z]-?\+?#?/)[0];
+    const duration = str.match(/\d+\.?/) ?? [currentNoteLength.toString()];
+    return [getFreq(noteName, currentOctave), getDurationInMS(duration[0], tempo)];
+}
+
+function getDurationInMS(durationAsStr, tempoInBPM) {
+    const crotchetDurationInMS = 60000 / tempoInBPM;
+    const noteDivisor = durationAsStr.includes('.') ? parseInt(durationAsStr.slice(0, -1)) / 1.5 : parseInt(durationAsStr);
+    const noteLengthInCrotchets = 4 / noteDivisor;
+    return Math.round(crotchetDurationInMS * noteLengthInCrotchets);
+}
+
+function getFreq(noteAsStr, currentOctave) {
+    if (noteAsStr === 'R' || noteAsStr === 'P') return 0;
+
+    const referenceFreq = 440;
+    const offset = getOffsetFromConcertA(noteAsStr, currentOctave);
+    return Math.round((2 ** (offset / 12) * referenceFreq) * 100) / 100;
+}
+
+function getOffsetFromConcertA(noteName, currentOctave) {
+    const notes = { 'A': 0, 'B': 2, 'C': 3, 'D': 5, 'E': 7, 'F': 8, 'G': 10 };
+
+    let accidental;
+    if (noteName.length === 1) {
+        accidental = 0;
+    }
+    else if (noteName[1] === '-') {
+        accidental = -1;
+    }
+    else if (noteName[1] === '#' || noteName[1] === '+') {
+        accidental = 1;
+    }
+
+    const semitonesAboveA = notes[noteName[0]] + accidental;
+    const semitonesInOctave = 12;
+
+    return semitonesAboveA + currentOctave * semitonesInOctave;
+}
