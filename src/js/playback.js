b79976eb9b99f7c2ceb70e479e5909acec1125ae
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-05-08 03:26:55 +0100

    Refactor playback to use mml-parser

--- src/js/playback.js
+++ src/js/playback.js
@@ -1,39 +1,97 @@
+import parseMml from 'mml-parser';
 import { Synth, Volume, now, Transport as player, start } from 'tone';
-import { MMLProcessor } from './note-processing.js';
 import { disableButtonsExceptStop, enableButtonsExceptStop, textareas } from './ui-controller';
 
+const ONE_SECOND = 1000;
 const Tone = { start, now, player, Volume, Synth };
+const tonePlayers = [];
 
 export function handlePlaybackControl(control) {
     switch (control) {
         case 'play':
             return play;
         case 'stop':
-            return;
+            return stop;
         default:
-            return () => {};
+            return null;
     }
 }
 
 async function play() {
-    await Tone.start();
-    disableButtonsExceptStop();
+    try {
+        await Tone.start();
+        disableButtonsExceptStop();
 
-    [...textareas]
-        .map((textarea) => textarea.value)
-        .filter((text) => text.length)
-        .forEach((text) => {
-            const processor = new MMLProcessor();
-            processor.loadNotesToTransport(text, Tone);
-        });
+        let maxLengthOfMusicInMs = 0;
 
-    Tone.player.start();
+        [...textareas]
+            .map((textarea) => textarea.value)
+            .filter((text) => text.length)
+            .forEach((text) => {
+                const lengthOfMusicLoaded = loadNotesToPlayer(text, Tone);
+                if (lengthOfMusicLoaded > maxLengthOfMusicInMs) {
+                    maxLengthOfMusicInMs = lengthOfMusicLoaded;
+                }
+            });
 
-    // * automatically handles buttons on play end and allows replayability
-    // * +1000ms to account for delay between pressing play and sound starting (so buttons change when sound actually ends)
-    setTimeout(() => {
+        Tone.player.start();
+
+        // automatically handles buttons on play end and allows replayability
+        // +1s to account for delay between pressing play and sound starting (so buttons change when sound actually ends)
+        setTimeout(() => {
+            Tone.player.stop();
+            enableButtonsExceptStop();
+        }, maxLengthOfMusicInMs + ONE_SECOND);
+    } catch (error) {
+        alert(error);
         enableButtonsExceptStop();
-        Tone.player.stop();
-        MMLProcessor.endTimeInMS = 0;
-    }, MMLProcessor.endTimeInMS + 1000);
+    }
+}
+
+function stop() {
+    tonePlayers.forEach((tonePlayer) => {
+        tonePlayer.synth.dispose();
+        tonePlayer.player.stop();
+        tonePlayer.player.clear(tonePlayer.playerId);
+    });
+
+    enableButtonsExceptStop();
+}
+
+function loadNotesToPlayer(text, toneController) {
+    const mmlTokens = parseMml(text);
+    if (!mmlTokens.length) {
+        return;
+    }
+
+    const volume = new toneController.Volume(-15).toDestination();
+    const synth = new toneController.Synth().connect(volume);
+    let noteStartOffsetInS = toneController.now();
+
+    const playerId = toneController.player.scheduleOnce(() => {
+        mmlTokens.forEach((token) => {
+            synth.triggerAttackRelease(
+                token.pitchInHz,
+                toSeconds(token.lengthInMs),
+                noteStartOffsetInS
+            );
+            noteStartOffsetInS += toSeconds(token.lengthInMs);
+        });
+    });
+
+    tonePlayers.push({
+        synth: synth,
+        player: toneController.player,
+        playerId: playerId,
+    });
+
+    return toMilliseconds(noteStartOffsetInS);
+}
+
+function toSeconds(milliseconds) {
+    return milliseconds / ONE_SECOND;
+}
+
+function toMilliseconds(seconds) {
+    return seconds * ONE_SECOND;
 }
