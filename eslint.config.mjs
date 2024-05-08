448dab96e9d45e36ffafc88d7d153c12da9c3fcc
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-05-08 03:24:19 +0100

    Remove unused files and update mml parser package

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import pluginJs from '@eslint/js';
 import globals from 'globals';
 
 export default [
-    { languageOptions: { globals: globals.browser } },
+    { languageOptions: { globals: [globals.browser, globals.node] } },
     { ignores: ['dist/'] },
     pluginJs.configs.recommended,
 ];
